46c5aaf555dbc788e4271bf611ac8bb6f1c99508
Vinh <31700818+ruanshiron@users.noreply.github.com>
2019-09-03 23:48:14 +0700

    3rd commit

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -8,150 +8,56 @@ export default function Portfolio(props) {
 
                 <div className="twelve columns collapsed">
 
-                    <h1>Check Out Some of My Works.</h1>
+                    <h1>I don't have much things to show here</h1>
 
                     <div id="portfolio-wrapper" className="bgrid-thirds s-bgrid-thirds cf">
-
-                    <div className="columns portfolio-item">
-                        <div className="item-wrap">
-
-                            <a href="#modal-01" title="">
-                                <img alt="" src="images/portfolio/coffee.jpg"/>
-                                <div className="overlay">
-                                    <div className="portfolio-item-meta">
-                                        <h5>Coffee</h5>
-                                        <p>Illustrration</p>
+                    {
+                        props.data.projects.map((p, i) => {
+                            return (
+                                <div className="columns portfolio-item">
+                                    <div className="item-wrap">
+            
+                                        <a href={"#modal-"+i} title="">
+                                            <img alt="" src={p.img}/>
+                                            <div className="overlay">
+                                                <div className="portfolio-item-meta">
+                                                    <h5>{p.name}</h5>
+                                                    <p>{p.desciption}</p>
+                                                </div>
+                                            </div>
+                                            <div className="link-icon"><i className="icon-plus"></i></div>
+                                        </a>
+            
                                     </div>
-                                </div>
-                                <div className="link-icon"><i className="icon-plus"></i></div>
-                            </a>
-
-                        </div>
-                    </div> 
-
-                    <div className="columns portfolio-item">
-                        <div className="item-wrap">
-
-                            <a href="#modal-02" title="">
-                                <img alt="" src="images/portfolio/console.jpg"/>
-                                <div className="overlay">
-                                <div className="portfolio-item-meta">
-                                        <h5>Console</h5>
-                                    <p>Web Development</p>
-                                    </div>
-                                </div>
-                                <div className="link-icon"><i className="icon-plus"></i></div>
-                            </a>
-
-                        </div>
-                        </div> 
-
-                    <div className="columns portfolio-item">
-                        <div className="item-wrap">
-
-                            <a href="#modal-03" title="">
-                                <img alt="" src="images/portfolio/judah.jpg"/>
-                                <div className="overlay">
-                                <div className="portfolio-item-meta">
-                                        <h5>Judah</h5>
-                                    <p>Webdesign</p>
-                                    </div>
-                                </div>
-                                <div className="link-icon"><i className="icon-plus"></i></div>
-                            </a>
-
-                        </div>
-                        </div>
-
-                    <div className="columns portfolio-item">
-                        <div className="item-wrap">
-
-                            <a href="#modal-04" title="">
-                                <img alt="" src="images/portfolio/into-the-light.jpg"/>
-                                <div className="overlay">
-                                <div className="portfolio-item-meta">
-                                        <h5>Into The Light</h5>
-                                    <p>Photography</p>
-                                    </div>
-                                </div>
-                                <div className="link-icon"><i className="icon-plus"></i></div>
-                            </a>
-
-                        </div>
-                        </div>
-
+                                </div> 
+                            )
+                        })
+                    }
                     </div> 
 
                 </div> 
+                {
+                    props.data.projects.map((p, i) => {
+                        return (
+                            <div id={"modal-"+i} className="popup-modal mfp-hide">
 
+                                <img className="scale-with-grid" src={p.img} alt="" />
 
-                <div id="modal-01" className="popup-modal mfp-hide">
+                                <div className="description-box">
+                                    <h4>{p.name}</h4>
+                                    <p>{p.detail}</p>
+                                <span className="categories"><i className="fa fa-tag"></i>{p.theme}</span>
+                                </div>
 
-                    <img className="scale-with-grid" src="images/portfolio/modals/m-coffee.jpg" alt="" />
+                                <div className="link-box">
+                                <a target="_blank" rel="noopener noreferrer"  href={p.github}>github</a>
+                                    <a className="popup-modal-dismiss">Close</a>
+                                </div>
 
-                    <div className="description-box">
-                        <h4>Coffee Cup</h4>
-                        <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit.</p>
-                    <span className="categories"><i className="fa fa-tag"></i>Branding, Webdesign</span>
-                    </div>
-
-                    <div className="link-box">
-                    <a href="http://www.behance.net">Details</a>
-                        <a className="popup-modal-dismiss">Close</a>
-                    </div>
-
-                </div>
-
-                <div id="modal-02" className="popup-modal mfp-hide">
-
-                    <img className="scale-with-grid" src="images/portfolio/modals/m-console.jpg" alt="" />
-
-                    <div className="description-box">
-                        <h4>Console</h4>
-                        <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit.</p>
-                    <span className="categories"><i className="fa fa-tag"></i>Branding, Web Development</span>
-                    </div>
-
-                    <div className="link-box">
-                    <a href="http://www.behance.net">Details</a>
-                        <a className="popup-modal-dismiss">Close</a>
-                    </div>
-
-                </div>
-
-                <div id="modal-03" className="popup-modal mfp-hide">
-
-                    <img className="scale-with-grid" src="images/portfolio/modals/m-judah.jpg" alt="" />
-
-                    <div className="description-box">
-                        <h4>Judah</h4>
-                        <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit.</p>
-                    <span className="categories"><i className="fa fa-tag"></i>Branding</span>
-                    </div>
-
-                    <div className="link-box">
-                    <a href="http://www.behance.net">Details</a>
-                        <a className="popup-modal-dismiss">Close</a>
-                    </div>
-
-                </div>
-
-                <div id="modal-04" className="popup-modal mfp-hide">
-
-                    <img className="scale-with-grid" src="images/portfolio/modals/m-intothelight.jpg" alt="" />
-
-                    <div className="description-box">
-                        <h4>Into the Light</h4>
-                        <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit.</p>
-                    <span className="categories"><i className="fa fa-tag"></i>Photography</span>
-                    </div>
-
-                    <div className="link-box">
-                    <a href="http://www.behance.net">Details</a>
-                        <a className="popup-modal-dismiss">Close</a>
-                    </div>
-
-                </div>
+                            </div>
+                        )
+                    })
+                }
 
             </div> 
 
--- src/data/resume.js
+++ src/data/resume.js
@@ -2,19 +2,71 @@ export const resume = {
     header: {
         name: 'Nguyen The Vinh',
         codename: 'ruanshiron',
-        someWords: 'Hey what\'s up',
+        someWords: 'An IT student at HUST',
         facebook: 'https://facebook.com/denlts.neo',
         instagram: 'https://instagram.com/ruanshiron',
         github: 'https://github.com/ruanshiron',
     },
     about: {
-
+        someWords: 'I\'m a HUST\'s student who wanna be a good IT engineer.'
     },
     resume: {
-
+        education: [
+            {
+                when: "September 2016 - Present",
+                where: "Hanoi University of Science and Technology",
+                who: "HEDSPI Student",
+                details: "Studying here is my opportunity to become a good engineer. Studying with better people makes me excited. I will graduated "
+            },
+            {
+                when: "September 2013 - June 2016",
+                where: "Ngoc Tao Highschool",
+                who: "Student",
+                details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc consectetur dignissim lectus non vestibulum. Vestibulum mi dolor, cursus et nulla quis, ultrices porttitor mauris. Maecenas in eleifend odio. Proin eu velit ac risus vehicula tincidunt. Donec mauris sem, molestie quis ex vitae, pretium cursus erat. Aliquam erat volutpat. Quisque enim enim, luctus at ligula id, condimentum blandit dolor. Phasellus dui elit, volutpat egestas vulputate at, semper ut eros. Proin eu arcu dictum, varius risus sed, sodales eros."
+            },
+            {
+                when: "September 2009 - June 2013",
+                where: "Tam Hiep Secondary School",
+                who: "Student",
+                details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc consectetur dignissim lectus non vestibulum. Vestibulum mi dolor, cursus et nulla quis, ultrices porttitor mauris. Maecenas in eleifend odio. Proin eu velit ac risus vehicula tincidunt. Donec mauris sem, molestie quis ex vitae, pretium cursus erat. Aliquam erat volutpat. Quisque enim enim, luctus at ligula id, condimentum blandit dolor. Phasellus dui elit, volutpat egestas vulputate at, semper ut eros. Proin eu arcu dictum, varius risus sed, sodales eros."
+            },
+        ],
+        intern: [
+            {
+                when: "July 2018 - November 2018",
+                where: "Ominext JSC",
+                who: "iOS Intern",
+                details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc consectetur dignissim lectus non vestibulum. Vestibulum mi dolor, cursus et nulla quis, ultrices porttitor mauris. Maecenas in eleifend odio. Proin eu velit ac risus vehicula tincidunt. Donec mauris sem, molestie quis ex vitae, pretium cursus erat. Aliquam erat volutpat. Quisque enim enim, luctus at ligula id, condimentum blandit dolor. Phasellus dui elit, volutpat egestas vulputate at, semper ut eros. Proin eu arcu dictum, varius risus sed, sodales eros."
+            }
+        ]
     },
     portfolio: {
-
+        projects: [
+            {
+                name: "CSDL",
+                desciption: "ReactJS, PostgreSQL, Node/express.js",
+                detail: "A project for Databass subject by teamwork(4 people)",
+                img: "images/portfolio/coffee.jpg",
+                theme: "Recipes, Cooking",
+                github: "https://github.com/ruanshiron/csdl"
+            },
+            {
+                name: "Sorting Demonstration",
+                desciption: "Java, JFoenix",
+                detail: "A project",
+                img: "images/portfolio/console.jpg",
+                theme: "Theory, Sorting, Visualization",
+                github: "https://github.com/ruanshiron/Sorting-Demonstration"
+            },
+            {
+                name: "Oumiji",
+                desciption: "Swift, Affdex, Kairos",
+                detail: "Intership",
+                img: "images/portfolio/girl.jpg",
+                theme: "Emotion, Face, Detection, Questionnaire",
+                github: "https://github.com/ruanshiron/csdl"
+            }
+        ]
     },
 
 }
